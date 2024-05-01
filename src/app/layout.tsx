987c4e2968b96126ed64533d0e28f1c178cfedef
mo3tafa <visapick.it3@gmail.com>
2024-05-01 08:16:39 +0330

    fix:delete extra files and codes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import { ReactNode } from "react";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+
+import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
